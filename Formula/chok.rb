14c7baae192a46b840769d52cde967c5a62688d4
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2024-03-20 22:08:06 +0900

    add bin

--- Formula/chok.rb
+++ Formula/chok.rb
@@ -13,8 +13,9 @@ class Chok < Formula
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "./configure", "--disable-silent-rules", *std_configure_args
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "chok"
   end
 
   test do
